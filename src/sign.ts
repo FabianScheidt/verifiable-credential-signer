9b9de3629d3b7110643672d539d511090911aca3
Scheidt, Fabian <fabian.scheidt@accenture.com>
2023-10-24 14:56:10 +0200

    feat: ensure that context for json web signature 2020 is present

--- src/sign.ts
+++ src/sign.ts
@@ -1,8 +1,9 @@
 import { JWK, JWS } from "node-jose";
 import { canonize, JsonLdDocument } from "jsonld";
 import * as crypto from "crypto";
+import { JsonLdObj } from "jsonld/jsonld-spec";
 
-interface Proof {
+interface JsonWebSignature2020Proof {
   type: "JsonWebSignature2020";
   created: string;
   proofPurpose: "assertionMethod";
@@ -10,13 +11,27 @@ interface Proof {
   jws: string;
 }
 
-export async function signVerifiableCredential<
-  T extends Record<string, unknown>,
->(
+export async function signVerifiableCredential<T extends JsonLdObj>(
   pemPrivateKey: string,
   verificationMethod: string,
   verifiableCredential: T,
-): Promise<T & { proof: Proof }> {
+): Promise<T & { proof: JsonWebSignature2020Proof }> {
+  // Drop potentially existing proof
+  delete verifiableCredential.proof;
+
+  // Ensure that VC context contains JsonWebKey2020
+  if (!(await checkForJsonWebKey2020Context(verifiableCredential))) {
+    const proofContext = "https://w3id.org/security/suites/jws-2020/v1";
+    const vcContext = verifiableCredential["@context"] ?? [];
+    const vcContextArray = Array.isArray(vcContext)
+      ? [...vcContext, proofContext]
+      : [vcContext, proofContext];
+    verifiableCredential = {
+      ...verifiableCredential,
+      "@context": vcContextArray,
+    };
+  }
+
   // Normalize VC
   const credentialNormalized = await normalize(verifiableCredential);
   const credentialHashed = await hash(credentialNormalized);
@@ -25,24 +40,34 @@ export async function signVerifiableCredential<
   const keystore = JWK.createKeyStore();
   const key = await keystore.add(pemPrivateKey, "pem");
 
-  // Sign and add signature
+  // Determine signature
   const signOptions: JWS.SignOptions = { format: "compact", alg: "PS256" };
   const credentialJws = await JWS.createSign(signOptions, key)
     .update(credentialHashed, "utf8")
     .final();
 
-  return {
-    ...verifiableCredential,
-    proof: {
-      type: "JsonWebSignature2020",
-      created: new Date().toISOString(),
-      proofPurpose: "assertionMethod",
-      verificationMethod: verificationMethod,
-      // Sign result is actually a string if the compact format is used:
-      // https://github.com/cisco/node-jose#signing-content
-      jws: credentialJws as unknown as string,
-    },
-  };
+  // Sign result is actually a string if the compact format is used:
+  // https://github.com/cisco/node-jose#signing-content
+  const credentialJwsStr = credentialJws as unknown as string;
+
+  // Add proof and return result
+  return addJsonWebSignature2020Proof(
+    verifiableCredential,
+    verificationMethod,
+    credentialJwsStr,
+  );
+}
+
+async function checkForJsonWebKey2020Context(
+  verifiableCredential: JsonLdObj,
+): Promise<boolean> {
+  // There is probably a smarter solution, but here we simply add the required keys and see if the validation fails.
+  try {
+    await normalize(addJsonWebSignature2020Proof(verifiableCredential, "", ""));
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
 
 async function normalize(payload: JsonLdDocument) {
@@ -60,4 +85,21 @@ async function hash(payload: string) {
   return Array.from(digestArray)
     .map((b) => b.toString(16).padStart(2, "0"))
     .join("");
+}
+
+function addJsonWebSignature2020Proof<T extends JsonLdObj>(
+  verifiableCredential: T,
+  verificationMethod: string,
+  jws: string,
+): T & { proof: JsonWebSignature2020Proof } {
+  return {
+    ...verifiableCredential,
+    proof: {
+      type: "JsonWebSignature2020",
+      created: new Date().toISOString(),
+      proofPurpose: "assertionMethod",
+      verificationMethod,
+      jws,
+    },
+  };
 }
